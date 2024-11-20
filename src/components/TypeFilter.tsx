0fa1c834b41321c83b21f8877f6dd015cdd03ce6
THEKaminsky <rjkaminskyjr@gmail>
2024-11-20 15:58:51 -0700

    Add sidebar with buttons for filtering on pokemon type

--- src/components/TypeFilter.tsx
+++ src/components/TypeFilter.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const SidebarContainer = styled.div`
   width: 200px;
-  padding: 20px;
+  padding: 0 20px;
   background-color: #f5f5f5;
   height: 100vh;
   position: fixed;
